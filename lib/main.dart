de52d15e54915535226490ab4e5f93d102fde551
Alexey Inkin <leha@inkin.ru>
2022-05-01 20:21:42 +0400

    Use url_strategy package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app_state/app_state.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'firebase_options.dart'; // Generated with CLI installation, https://firebase.flutter.dev/docs/cli
 import 'locator.dart';
@@ -15,6 +16,7 @@ import 'theme/theme.dart';
 final pageStacksBloc = PageStacksBloc();
 
 void main() async {
+  setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
